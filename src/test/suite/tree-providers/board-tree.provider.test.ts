c4265482fca955618c409ded6ac3986fb6c2ad84
bernd <bernd_fuhrmann@gmx.de>
2024-06-16 18:48:54 +0100

    improve test

--- src/test/suite/tree-providers/board-tree.provider.test.ts
+++ src/test/suite/tree-providers/board-tree.provider.test.ts
@@ -6,49 +6,46 @@ import * as vscode from 'vscode';
 import { BoardService } from '../../../api/services/board.service';
 import { WorkItemService } from '../../../api/services/work-item.service';
 import { BoardsTreeProvider } from '../../../tree-providers/board-tree.provider';
-import { AppSettingsService } from '../../../services/app-settings.service';
+import { mockAppSettingService } from '../../helpers/mockAppSettingService';
+import { BoardItem } from '../../../tree-items/board-item.class';
 
 suite('BoardTreeProvider', () => {
 	test('invalid configuration returns empty list', () => {
 		const context = {} as vscode.ExtensionContext;
-		const appSettingsService = {
-			getProject() {
-				return 'test-project';
-			},
-			getTeam() {
-				return 'test-team';
-			},
-			getTeamContext() {
-				return {
-					project: this.getProject(),
-					team: this.getTeam(),
-				};
-			},
-			isValidAppSettings() {
-				return false;
-			},
-			getServerUrl() {
-				return 'test-url';
-			},
-			getPersonalAccessToken() {
-				return 'test-token';
-			},
-			getOrganization() {
-				return 'test-organization';
-			},
-			getIteration() {
-				return 'test-iteration';
-			},
-		} as AppSettingsService;
+		const appSettingsService = mockAppSettingService;
 		const boardService = {} as BoardService;
 		const workItemService = {} as WorkItemService;
 		const subject = new BoardsTreeProvider(
 			context,
-			appSettingsService,
+			appSettingsService(false),
 			boardService,
 			workItemService,
 		);
 		const children = subject.getChildren();
 		assert.deepEqual(children, []);
+	});
+
+	test('return boards', async () => {
+		const context = {} as vscode.ExtensionContext;
+		const appSettingsService = mockAppSettingService;
+		const boardService = {
+			getAll: async () => {
+				return [{
+					name: 'test-board1',
+					id: 'test-id1',
+					url: 'test-board-url1'
+				}];
+			} 
+		} as BoardService;
+		const workItemService = {} as WorkItemService;
+		const subject = new BoardsTreeProvider(
+			context,
+			appSettingsService(true),
+			boardService,
+			workItemService,
+		);
+		const children = await (subject.getChildren() as Promise<BoardItem[]>);
+		assert.equal(children.length, 1);
+		assert.deepEqual(children[0].label, 'test-board1');
 	});
 });
